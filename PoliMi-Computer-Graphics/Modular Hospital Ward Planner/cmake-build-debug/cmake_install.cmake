30d92d9ec1138718e2e682d74fa4ee234d1f54b0
Smrevilo <74360058+Smrevilo@users.noreply.github.com>
2025-07-21 17:15:22 +0200

    Added the potted plants assets

--- PoliMi-Computer-Graphics/Modular Hospital Ward Planner/cmake-build-debug/cmake_install.cmake
+++ PoliMi-Computer-Graphics/Modular Hospital Ward Planner/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/oliverstege/Desktop/Polimi/Computer Graphics/A02
+# Install script for directory: /Users/oliverstege/Desktop/Polimi/NSDS/PoliMi-Computer-Graphics/PoliMi-Computer-Graphics/Modular Hospital Ward Planner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/oliverstege/Desktop/Polimi/Computer Graphics/A02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/oliverstege/Desktop/Polimi/NSDS/PoliMi-Computer-Graphics/PoliMi-Computer-Graphics/Modular Hospital Ward Planner/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
