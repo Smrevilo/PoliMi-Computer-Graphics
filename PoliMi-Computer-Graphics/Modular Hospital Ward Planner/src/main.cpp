2b088e0a0719523e0b0d43b09e40300bd53c57c4
Smrevilo <74360058+Smrevilo@users.noreply.github.com>
2025-07-23 16:00:47 +0200

    Implemented Isometric view

--- PoliMi-Computer-Graphics/Modular Hospital Ward Planner/src/main.cpp
+++ PoliMi-Computer-Graphics/Modular Hospital Ward Planner/src/main.cpp
@@ -391,10 +391,9 @@ class ModularHospitalWardPlanner : public BaseProject {
 		glm::mat4 M = glm::perspective(glm::radians(60.0f), Ar, 0.1f, 500.0f);
 		M[1][1] *= -1;
 
-		glm::mat4 Mv =  glm::inverse(glm::translate(glm::mat4(1), Pos) *
-										glm::rotate(glm::mat4(1), DlookAng, glm::vec3(0,1,0)) *
-										glm::translate(glm::mat4(1), glm::vec3(0,5,15))
-										);
+		glm::vec3 cameraOffset = glm::vec3(-15.0f, 15.0f, 15.0f);
+		glm::vec3 cameraPos = Pos + cameraOffset;
+		glm::mat4 Mv = glm::lookAt(cameraPos, Pos, glm::vec3(0,1,0));
 
 		glm::mat4 ViewPrj =  M * Mv;
 		UniformBufferObject ubo{};
